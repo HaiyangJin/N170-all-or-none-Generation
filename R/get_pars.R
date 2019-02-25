4926d860adec3e9982e47c8fc9813c52f398d374
Haiyang <hjin317@aucklanduni.ac.nz>
2019-02-26 11:02:20 +1300

    multiple updates
    add get_pars
    show RStudio version
    add lmm for RT E4

---
+++ R/get_pars.R
@@ -0,0 +1,14 @@
+# get parameters from the model (and will be applied to the start of the to-be-refitted model)
+# codes are mainly from https://rdrr.io/cran/lme4/man/convergence.html
+# Author: Haiyang Jin (https://haiyangjin.github.io/portfolio/)
+
+get_pars <- function(lmm) {
+    # lmm is the (generalized) linear mixed model
+    if (isLMM(lmm)) {
+        pars <- getME(lmm,"theta")
+    } else {
+        ## GLMM: requires both random and fixed parameters
+        pars <- getME(lmm, c("theta","fixef"))
+    }
+    return(pars)
+} 
