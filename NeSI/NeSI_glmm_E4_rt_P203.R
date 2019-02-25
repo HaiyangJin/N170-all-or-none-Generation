a9209ea3384b7ab78cfa2d83a63e607a73b99af5
Haiyang <hjin317@aucklanduni.ac.nz>
2019-02-25 20:55:52 +1300

    more extended models for RT

--- NeSI/NeSI_glmm_E4_rt_P203.R
+++ NeSI/NeSI_glmm_E4_rt_P203.R
@@ -100,22 +100,41 @@ load("E204_beha_RT.RData")
 
 
 #############################  Fitting the glmm etd2 for response times  ##############################
+# # fit the glmm.etd2.rt.E4 model
+# message("")
+# message(paste0(strrep("#", 80)))
+# message("Fitting the glmm.etd2.rt.E4 model...")
+# 
+# # glmm.etd2.rt.E4 for mean amplitude
+# load("E204_rt_glmm_etd.RData")
+# glmm.etd2.rt.E4 <- update(glmm.etd.rt.E4,
+#                           formula = RT ~ Type * Category * Duration +
+#                             (1 + Type_D + Cate_D + Type_Cate + Type_Dura + Type_Cate_Dura | SubjCode) +
+#                             (0 + Type_D + Type_Dura + Type_Cate_Dura | Stimuli))
+# 
+# # Saving glmm.etd2.rt.E4
+# message("")
+# message("Saving the glmm.etd2.rt.E4")
+# save(glmm.etd2.rt.E4, file = "E204_rt_glmm_etd2.RData")
+
+
+#############################  Fitting the glmm etd3 for response times  ##############################
 # fit the glmm.etd2.rt.E4 model
 message("")
 message(paste0(strrep("#", 80)))
-message("Fitting the glmm.etd2.rt.E4 model...")
+message("Fitting the glmm.etd3.rt.E4 model...")
 
-# glmm.etd.rt.E4 for mean amplitude
-load("E204_rt_glmm_etd.RData")
-glmm.etd2.rt.E4 <- update(glmm.etd.rt.E4,
+# glmm.etd3.rt.E4 for mean amplitude
+load("E204_rt_glmm_etd2.RData")
+glmm.etd3.rt.E4 <- update(glmm.etd2.rt.E4,
                           formula = RT ~ Type * Category * Duration +
                             (1 + Type_D + Cate_D + Type_Cate + Type_Dura + Type_Cate_Dura | SubjCode) +
                             (0 + Type_D + Type_Dura + Type_Cate_Dura | Stimuli))
 
-# Saving glmm.etd2.rt.E4
+# Saving glmm.etd3.rt.E4
 message("")
-message("Saving the glmm.etd2.rt.E4")
-save(glmm.etd2.rt.E4, file = "E204_rt_glmm_etd2.RData")
+message("Saving the glmm.etd3.rt.E4")
+save(glmm.etd3.rt.E4, file = "E204_rt_glmm_etd3.RData")
 
 
 # versions of packages used
