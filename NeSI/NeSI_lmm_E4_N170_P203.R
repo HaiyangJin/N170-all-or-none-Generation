f9f106744732be68579e92ae6bbd2a7ed1da9822
Haiyang <hjin317@aucklanduni.ac.nz>
2019-03-18 21:32:32 +1300

    refit zcp for amplitudes

--- NeSI/NeSI_lmm_E4_N170_P203.R
+++ NeSI/NeSI_lmm_E4_N170_P203.R
@@ -47,40 +47,53 @@ load("E204_erp_N170.RData")
 
 
 #############################  Fitting the lmm.zcp.N170 for N170  ##############################
-# fit the lmm.zcp.N170 model
-message("")
-message(paste0(strrep("#", 80)))
-message("Fitting the lmm.zcp.N170 model...")
-
-# lmm.zcp.N170 for mean amplitude
-load("E204_N170_lmm_max.RData")
-lmm.zcp.N170.E4 <- update(lmm.max.N170.E4,
-                          formula = MeanAmp ~ Type * Category * Duration * Hemisphere * ACC + 
-                            (1 + Type_D + Cate_D + Dura_D + Hemi_D + ACC_D + 
-                               Type_Cate + Type_Dura + Cate_Dura + Type_Hemi + Cate_Hemi + Dura_Hemi + Type_ACC + Cate_ACC + Dura_ACC + Hemi_ACC +
-                               Type_Cate_Dura + Type_Cate_Hemi + Type_Dura_Hemi + Cate_Dura_Hemi + Type_Cate_ACC + Type_Dura_ACC + Cate_Dura_ACC +
-                               Type_Hemi_ACC + Cate_Hemi_ACC + Dura_Hemi_ACC +
-                               Type_Cate_Dura_Hemi + Type_Cate_Dura_ACC + Type_Cate_Hemi_ACC + Type_Hemi_Dura_ACC + Hemi_Cate_Dura_ACC +
-                               Type_Cate_Dura_Hemi_ACC || SubjCode))
-
-# Saving lmm.zcp.N170
-message("")
-message("Saving the lmm.zcp.N170.E4")
-save(lmm.zcp.N170.E4, file = "E204_N170_lmm_zcp.RData")
-
-#############################  Obtaining (step) the lmm.rdc.N170 for N170  ##############################
-# # reduce the lmm.zcp.n170 model
+# # fit the lmm.zcp.N170 model
 # message("")
 # message(paste0(strrep("#", 80)))
-# message("Reducing the lmm.zcp.n170 model...")
+# message("Fitting the lmm.zcp.N170 model...")
 # 
-# load("E204_N170_lmm_zcp.RData")
-# lmm.zcp.N170.E4.step <- step(lmm.zcp.N170.E4, reduce.fixed = FALSE)
+# # lmm.zcp.N170 for mean amplitude
+# load("E204_N170_lmm_max.RData")
+# # lmm.zcp.N170.E4 <- update(lmm.max.N170.E4,
+# #                           formula = MeanAmp ~ Type * Category * Duration * Hemisphere * ACC +
+# #                             (1 + Type_D + Cate_D + Dura_D + Hemi_D + ACC_D +
+# #                                Type_Cate + Type_Dura + Cate_Dura + Type_Hemi + Cate_Hemi + Dura_Hemi + Type_ACC + Cate_ACC + Dura_ACC + Hemi_ACC +
+# #                                Type_Cate_Dura + Type_Cate_Hemi + Type_Dura_Hemi + Cate_Dura_Hemi + Type_Cate_ACC + Type_Dura_ACC + Cate_Dura_ACC +
+# #                                Type_Hemi_ACC + Cate_Hemi_ACC + Dura_Hemi_ACC +
+# #                                Type_Cate_Dura_Hemi + Type_Cate_Dura_ACC + Type_Cate_Hemi_ACC + Type_Hemi_Dura_ACC + Hemi_Cate_Dura_ACC +
+# #                                Type_Cate_Dura_Hemi_ACC || SubjCode))
 # 
-# # Saving lmm.zcp.N170.step
+# lmm.zcp.N170.E4 <- lmer(MeanAmp ~ Type * Category * Duration * Hemisphere * ACC +
+#                           (1 + Type_D + Cate_D + Dura_D + Hemi_D + ACC_D +
+#                              Type_Cate + Type_Dura + Cate_Dura + Type_Hemi + Cate_Hemi + Dura_Hemi + Type_ACC + Cate_ACC + Dura_ACC + Hemi_ACC +
+#                              Type_Cate_Dura + Type_Cate_Hemi + Type_Dura_Hemi + Cate_Dura_Hemi + Type_Cate_ACC + Type_Dura_ACC + Cate_Dura_ACC +
+#                              Type_Hemi_ACC + Cate_Hemi_ACC + Dura_Hemi_ACC +
+#                              Type_Cate_Dura_Hemi + Type_Cate_Dura_ACC + Type_Cate_Hemi_ACC + Type_Hemi_Dura_ACC + Hemi_Cate_Dura_ACC +
+#                              Type_Cate_Dura_Hemi_ACC || SubjCode),
+#                         data = df.erp.N170.E4,
+#                         REML = FALSE,
+#                         verbose = TRUE,
+#                         control = lmerControl(optimizer = "bobyqa",  # nloptwrap Nelder_Mead
+#                                               optCtrl = list(maxfun = 1e7)))
+# 
+# # Saving lmm.zcp.N170
 # message("")
-# message("Saving the lmm.zcp.N170.E4.step")
-# save(lmm.zcp.N170.E4.step, file = "E204_N170_lmm_zcp_step.RData")
+# message("Saving the lmm.zcp.N170.E4")
+# save(lmm.zcp.N170.E4, file = "E204_N170_lmm_zcp.RData")
+
+#############################  Obtaining (step) the lmm.rdc.N170 for N170  ##############################
+# reduce the lmm.zcp.n170.E4 model
+message("")
+message(paste0(strrep("#", 80)))
+message("Reducing the lmm.zcp.N170.E4 model...")
+
+load("E204_N170_lmm_zcp.RData")
+lmm.zcp.N170.E4.step <- step(lmm.zcp.N170.E4, reduce.fixed = FALSE)
+
+# Saving lmm.zcp.N170.step
+message("")
+message("Saving the lmm.zcp.N170.E4.step")
+save(lmm.zcp.N170.E4.step, file = "E204_N170_lmm_zcp_step.RData")
 
 
 # versions of packages used
--- NeSI/NeSI_lmm_E4_P1_P203.R
+++ NeSI/NeSI_lmm_E4_P1_P203.R
@@ -47,43 +47,54 @@ load("E204_erp_P1.RData")
 
 
 #############################  Fitting the lmm.zcp.P1 for P1  ##############################
-# fit the lmm.zcp.P1 model
-message("")
-message(paste0(strrep("#", 80)))
-message("Fitting the lmm.zcp.P1.E4 model...")
-
-# lmm.zcp.P1 for mean amplitude
-load("E204_P1_lmm_max.RData")
-
-lmm.zcp.P1.E4 <- update(lmm.max.P1.E4,
-                        formula = MeanAmp ~ Type * Category * Duration * Hemisphere * ACC + 
-                          (1 + Type_D + Cate_D + Dura_D + Hemi_D + ACC_D + 
-                             Type_Cate + Type_Dura + Cate_Dura + Type_Hemi + Cate_Hemi + Dura_Hemi + Type_ACC + Cate_ACC + Dura_ACC + Hemi_ACC +
-                             Type_Cate_Dura + Type_Cate_Hemi + Type_Dura_Hemi + Cate_Dura_Hemi + Type_Cate_ACC + Type_Dura_ACC + Cate_Dura_ACC +
-                             Type_Hemi_ACC + Cate_Hemi_ACC + Dura_Hemi_ACC +
-                             Type_Cate_Dura_Hemi + Type_Cate_Dura_ACC + Type_Cate_Hemi_ACC + Type_Hemi_Dura_ACC + Hemi_Cate_Dura_ACC +
-                             Type_Cate_Dura_Hemi_ACC || SubjCode))
-
-
-# Saving lmm.zcp.P1
-message("")
-message("Saving the lmm.zcp.P1.E4")
-save(lmm.zcp.P1.E4, file = "E204_P1_lmm_zcp.RData")
-
-
-#############################  Obtaining (step) the lmm.rdc.P1 for P1  ##############################
-# # reduce the lmm.zcp.P1 model
+# # fit the lmm.zcp.P1 model
 # message("")
 # message(paste0(strrep("#", 80)))
-# message("Reducing the lmm.zcp.P1 model...")
+# message("Fitting the lmm.zcp.P1.E4 model...")
 # 
-# load("E204_P1_lmm_zcp.RData")
-# lmm.zcp.P1.E4.step <- step(lmm.zcp.P1.E4, reduce.fixed = FALSE)
+# # lmm.zcp.P1 for mean amplitude
+# load("E204_P1_lmm_max.RData")
 # 
-# # Saving lmm.zcp.P1.step
+# # lmm.zcp.P1.E4 <- update(lmm.max.P1.E4,
+# #                         formula = MeanAmp ~ Type * Category * Duration * Hemisphere * ACC +
+# #                           (1 + Type_D + Cate_D + Dura_D + Hemi_D + ACC_D +
+# #                              Type_Cate + Type_Dura + Cate_Dura + Type_Hemi + Cate_Hemi + Dura_Hemi + Type_ACC + Cate_ACC + Dura_ACC + Hemi_ACC +
+# #                              Type_Cate_Dura + Type_Cate_Hemi + Type_Dura_Hemi + Cate_Dura_Hemi + Type_Cate_ACC + Type_Dura_ACC + Cate_Dura_ACC +
+# #                              Type_Hemi_ACC + Cate_Hemi_ACC + Dura_Hemi_ACC +
+# #                              Type_Cate_Dura_Hemi + Type_Cate_Dura_ACC + Type_Cate_Hemi_ACC + Type_Hemi_Dura_ACC + Hemi_Cate_Dura_ACC +
+# #                              Type_Cate_Dura_Hemi_ACC || SubjCode))
+# lmm.zcp.P1.E4 <- lmer(MeanAmp ~ Type * Category * Duration * Hemisphere * ACC +
+#                         (1 + Type_D + Cate_D + Dura_D + Hemi_D + ACC_D +
+#                            Type_Cate + Type_Dura + Cate_Dura + Type_Hemi + Cate_Hemi + Dura_Hemi + Type_ACC + Cate_ACC + Dura_ACC + Hemi_ACC +
+#                            Type_Cate_Dura + Type_Cate_Hemi + Type_Dura_Hemi + Cate_Dura_Hemi + Type_Cate_ACC + Type_Dura_ACC + Cate_Dura_ACC +
+#                            Type_Hemi_ACC + Cate_Hemi_ACC + Dura_Hemi_ACC +
+#                            Type_Cate_Dura_Hemi + Type_Cate_Dura_ACC + Type_Cate_Hemi_ACC + Type_Hemi_Dura_ACC + Hemi_Cate_Dura_ACC +
+#                            Type_Cate_Dura_Hemi_ACC || SubjCode),
+#                       data = df.erp.P1.E4,
+#                       REML = FALSE,
+#                       verbose = TRUE,
+#                       control = lmerControl(optimizer = "bobyqa",  # nloptwrap Nelder_Mead
+#                                             optCtrl = list(maxfun = 1e7)))
+# 
+# # Saving lmm.zcp.P1
 # message("")
-# message("Saving the lmm.zcp.P1.E4.step")
-# save(lmm.zcp.P1.E4.step, file = "E204_P1_lmm_zcp_step.RData")
+# message("Saving the lmm.zcp.P1.E4")
+# save(lmm.zcp.P1.E4, file = "E204_P1_lmm_zcp.RData")
+
+
+#############################  Obtaining (step) the lmm.zcp.P1.E4 for P1  ##############################
+# reduce the lmm.zcp.P1 model
+message("")
+message(paste0(strrep("#", 80)))
+message("Reducing the lmm.zcp.P1.E4 model...")
+
+load("E204_P1_lmm_zcp.RData")
+lmm.zcp.P1.E4.step <- step(lmm.zcp.P1.E4, reduce.fixed = FALSE)
+
+# Saving lmm.zcp.P1.step
+message("")
+message("Saving the lmm.zcp.P1.E4.step")
+save(lmm.zcp.P1.E4.step, file = "E204_P1_lmm_zcp_step.RData")
 
 
 # versions of packages used
